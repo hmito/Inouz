d8db91856f123b3ecdcc081fbf9369944613e8f6
Koichi Ito <hmito@outlook.com>
2020-03-11 19:46:18 -0700

    Add hmLib

---
+++ Inouz/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<future>
+#include<chrono>
+#include"hmLib/coordinates.hpp"
+struct inertial_device {
+
+};
+struct inertial_guidance {
+	hmLib::coordinates3D::angle Angle;
+};
+int main(){
+	return 0;
+}
